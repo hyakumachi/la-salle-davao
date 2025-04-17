ec07ad89033d6ee8624b35d90fcd39bc82953ba2
Bernard Louie <pacanza.berns4884@gmail.com>
2025-04-17 22:11:05 +0800

    fixed copyright html entity

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,16 @@
-import React from 'react'
-import './Footer.css'
-
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <p>
-            Copyright &copy 2025. Bernard Pacanza. All rights reserved.
-        </p>
-        <ul>
-            <li>
-                Terms of services
-            </li>
-            <li>
-                Privacy policy
-            </li>
-        </ul>
+    <div className="footer">
+      <p>Copyright &copy; 2025. Bernard Pacanza. All rights reserved.</p>
+      <ul>
+        <li>Terms of services</li>
+        <li>Privacy policy</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
